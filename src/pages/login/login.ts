2f6874b3c2e44c59b3d587443b79d730d17abd08
guilhermeikeda <guilherme@opus-software.com.br>
2016-12-01 13:05:44 -0200

    Finished navigation

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, MenuController } from 'ionic-angular';
 import { Page1 } from '../page1/page1';
 
-/*
-  Generated class for the Login page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
